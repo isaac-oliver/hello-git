2e51f399968e4e44fad783ab1f1cd18b9bc02ab1
Isaac Oliver <isaac051307@gmail.com>
2026-06-16 13:56:02 +0800

    Testing Branching

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ name = "Isaac Oliver"
 age = 19
 print("My name is " + name + " and I am " + str(age) + " years old.")
 
+if age >= 18:
+    print("I am an adult.")
+else:    print("I am a kid.")
 
+print("Testing git branching and merging.")
